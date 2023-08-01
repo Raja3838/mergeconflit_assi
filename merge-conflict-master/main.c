d19af1a6378f8c908ecaf978f0ee5b706c8ae449
Raja Kotipalli <66884043+Raja3838@users.noreply.github.com>
2023-08-01 19:10:39 +0200

    git_mergeconflict_comit
    git_mergeconflict_comit1

---
+++ merge-conflict-master/main.c
@@ -0,0 +1,25 @@
+pseudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+
+feature1()
+{
+  //Sample Code for Feate
+  printf("feature1 change applied");
+}
+
+feature2()
+{
+  //Sample Code for Feature 2
+  printf("feature1");
+}
+
+Initial Security()
+{
+ //Sample Code
+ printf("Secured with SSL!");
+}
